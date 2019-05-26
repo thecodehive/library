a6c2ec7be41f676d1d529da82fb7bbada44ded8a
= <=>
2019-05-26 22:03:51 +0300

    Adds powerpoint & db sql file

---
+++ assets/test.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 26, 2019 at 09:00 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `pass` varchar(200) NOT NULL,
+  `email` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `pass`, `email`) VALUES
+(1, 'admin', 'admin.com', 'admin@gmail.com'),
+(2, 'abel', 'abel.com', 'abel@ymail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,39 @@
+<?php
+
+$name = '';
+
+if (isset($_GET['id'])) {
+	$id = $_GET['id'];
+
+
+	include 'config/db.php';
+
+	$sql = "SELECT name FROM user WHERE id = $id";
+
+	$query = mysqli_query($con, $sql);
+
+	if ($query) {
+		while ($row = mysqli_fetch_assoc($query)) {
+			$name = $row['name'];
+		}
+	}
+	elseif(mysqli_error($con)){
+		echo "<h4 style='padding:1rem; background:#eee; color:coral; margin:0 auto; width:250px;'>".mysqli_error($con)."</h4>";
+	}
+}
+
+
+/*
+The following is an example of a legitimate HTTP request that could be made to the vulnerable application above.
+
+http://localhost/library/dashboard.php?id=1
+> admin
+
+The following is an example of a malicious HTTP request that could be made to the vulnerable application above.
+http://localhost/library/dashboard.php?id=-1 UNION SELECT pass FROM user where id=1
+> admin.com
+*/
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +43,8 @@
 <body>
 	<div class="login-page">
 	  <div class="form">
-	    <h3>Welcome to the Dashboard</h3>
+	    <h2>Hello, <span style="color: magenta;"><?php echo $name ?></span></h2>
+	    <h3><br>Welcome to the dashboard</h3>
 	  	<a href="register.php">Log out</a>
 	  </div>
 	</div>
